a2fd6da084c0c322cbbc32129d00bf8587171f64
Ravi <ravikishore030@gmail.com>
2021-12-26 14:20:37 +0530

    With iterator

--- tfcode/dynamicrules.tf
+++ tfcode/dynamicrules.tf
@@ -22,3 +22,28 @@ resource "oci_core_security_list" "test_security_list" {
         }
     }
 }
+
+
+
+
+
+#### with iterator.
+resource "oci_core_security_list" "test_security_list" {
+    #Required
+    compartment_id = "<>"
+    vcn_id = "<>"
+    display_name = "dynamic_rules"
+    dynamic "ingress_security_rules" {
+         for_each = var.portslist
+         iterator = rule
+         content{
+            source      = "10.0.0.0/24"
+            source_type = "CIDR_BLOCK"
+            protocol    = "6"
+            tcp_options {
+                min = rule.value
+                max = rule.value
+            }
+        }
+    }
+}
