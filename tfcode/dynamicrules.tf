23e3c8ef00e07583ea85511dac276eecdd51c83a
Ravi <ravikishore030@gmail.com>
2021-12-26 11:55:43 +0530

    Create dynamicrules.tf

---
+++ tfcode/dynamicrules.tf
@@ -0,0 +1,24 @@
+variable "portslist" {
+    type = list(number)
+    default = [22, 80, 443, 7000, 8080, 9009, 9000]
+  
+}
+
+resource "oci_core_security_list" "test_security_list" {
+    #Required
+    compartment_id = "<>"
+    vcn_id = "<>"
+    display_name = "dynamic_rules"
+    dynamic "ingress_security_rules" {
+         for_each = var.portslist
+         content{
+            source      = "10.0.0.0/24"
+            source_type = "CIDR_BLOCK"
+            protocol    = "6"
+            tcp_options {
+                min = ingress_security_rules.value
+                max = ingress_security_rules.value
+            }
+        }
+    }
+}
